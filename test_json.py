ac7f68187684c96b5273f8c3d086291bee893986
Mark <wancy86@sina.com>
2016-04-14 18:33:34 +0800

    'json'

--- test_json.py
+++ test_json.py
@@ -1,13 +1,13 @@
-
+# need to run with django
 class Category(models.Model):
     autoid = models.AutoField(primary_key=True)
-    email=models.CharField(max_length=150,blank=False)
-    comtype=models.CharField(max_length=20,blank=False)
-    catname=models.CharField(max_length=150,blank=False)  
-    
+    email = models.CharField(max_length=150, blank=False)
+    comtype = models.CharField(max_length=20, blank=False)
+    catname = models.CharField(max_length=150, blank=False)
+
     def __unicode__(self):
         return '%s' % (self.catname)
-    
+
     def toJSON(self):
         import json
         return json.dumps(dict([(attr, getattr(self, attr)) for attr in [f.name for f in self._meta.fields]]))
