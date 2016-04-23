ac080a09902b30659b0345967c882d0d421792b1
Mark <wancy86@sina.com>
2016-04-23 19:27:49 +0800

    test

--- pp.py
+++ pp.py
@@ -1,8 +1,26 @@
-import uuid
-print(uuid.uuid4())
-#dad958592e9d43e59b5c4dd3e6eb4707
+# import uuid
+# print(uuid.uuid4())
+# # dad958592e9d43e59b5c4dd3e6eb4707
 
 
+class Pclass(object):
+    """docstring for Pclass"""
+    num = 10
 
+    def __init__(self):
+        super(Pclass, self).__init__()
 
+p = Pclass()
+print(p.num)    #10
+p.num = p.num + 1
+print(p.num)    #11
+print(Pclass.num)   #10
+
+Pclass.age=12
+print(Pclass.age)   #12
+print(p.age)    #12
+
+p.xxx=555
+print(p.xxx) #555
+print(Pclass.xxx) #AttributeError: type object 'Pclass' has no attribute 'xxx'
 
